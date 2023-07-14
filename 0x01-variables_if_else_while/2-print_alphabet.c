fd2efe0f07fa29a1ac665b21b1c8e3775b54e0a0
ShivaliSewcharran <shivalisewcharran@gmail.com>
2023-07-14 12:08:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stduio.h>
+/**
+  *main - Entry point
+  *Description: 'Prints alphabets in lower case'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
