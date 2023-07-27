7f47a414e01d2c5d39ce4a7b76c9e261eee901fe
ShivaliSewcharran <shivalisewcharran@gmail.com>
2023-07-27 08:44:33 -0700

    444

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -33,8 +33,8 @@ void print_buffer(char *b, int size)
 			{
 				printf(" ");
 			}
-
-			for (i = 0; i < j; i++)
+		}
+		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
 
@@ -46,6 +46,5 @@ void print_buffer(char *b, int size)
 		}
 		printf("\n");
 		o += 10;
-		}
 	}
 }
