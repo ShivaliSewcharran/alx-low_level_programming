fe3a6df6a4565dc2269f2e973786073f0a36d7e9
ShivaliSewcharran <shivalisewcharran@gmail.com>
2023-08-04 14:17:20 -0700

    0x09-static_libraries

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -2,8 +2,8 @@
 
 
 /**
- * _putchar - writes the char c to stdout
- * @c: The char to be printed
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
